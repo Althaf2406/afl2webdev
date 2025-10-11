6fefd86d40c2b25687bf20c1d1b90cee0335f4b3
Lui <lnathaniel@student.ciputra.ac.id>
2025-10-11 19:52:19 +0700

    migration seeder

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/corporate-governance', function () {
+    return view('corporate_governance');
+});
+
+Route::get('/mining-assets', function () {
+    return view('mining_assets');
+});
+
+Route::get('/project-data', function () {
+    return view('project_data');
+});
+
+Route::get('/investor-relation', function () {
+    return view('investor_relation');
+});

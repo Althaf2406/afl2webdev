b2c230d4d8f963cd1533999c8808c43472709c3b
Lui <lnathaniel@student.ciputra.ac.id>
2025-11-08 21:48:46 +0700

    ya

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,5 @@ Route::put('/galeri/update/{id}', [GaleriController::class, 'update']);
 
 Route::delete('/galeri/delete/{id}', [GaleriController::class, 'destroy']);
 
-Route::get('/mining-assets', [PageController::class, 'mining_assets']);
-
-Route::get('/project-data', [PageController::class, 'project_data']);
-
-Route::get('/investor-relation', [PageController::class, 'investor_relation']);
 
 ?>
